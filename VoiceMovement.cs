e0b15149cce2caa25ddfebeb46fef391d9ca654f
brightcxy <47863584+brightcxy@users.noreply.github.com>
2021-06-09 14:47:59 +0800

    Add files via upload

---
+++ VoiceMovement.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using UnityEngine;
+using UnityEngine.Windows.Speech;
+
+public class VoiceMovement : MonoBehaviour
+{
+
+    private KeywordRecognizer keywordRecognizer;
+    private Dictionary<string, Action> actions = new Dictionary<string, Action>();
+
+    void Start()
+    {
+        actions.Add("forward", Forward);
+        actions.Add("Create", Create);
+        actions.Add("Destroy", Destroy);
+        actions.Add("back", Back);
+
+        keywordRecognizer = new KeywordRecognizer(actions.Keys.ToArray());
+        keywordRecognizer.OnPhraseRecognized += RecognizedSpeech;
+        keywordRecognizer.Start();
+    }
+
+    private void RecognizedSpeech(PhraseRecognizedEventArgs speech)
+    {
+        Debug.Log(speech.text);
+        actions[speech.text].Invoke();
+    }
+
+    private void Forward()
+    {
+        transform.Translate(1, 0, 0);
+
+    }
+
+    private void Back()
+    {
+        transform.Translate(-1, 0, 0);
+    }
+
+    private void Create()
+    {
+        transform.Translate(0, 10, 0);
+    }
+
+    private void Destroy()
+    {
+        transform.Translate(0, -10, 0);
+    }
+}
